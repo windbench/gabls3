4fa575a8fa44d8ae1b500ef403fd8ec80f50b2c8
windbench <pgancarski@cener.com>
2017-04-20 18:37:41 +0200

    Changed all
    first working version

--- eudat_connect.sh
+++ eudat_connect.sh
@@ -1,1 +1,1 @@
-mount.davfs https://b2drop.eudat.eu/remote.php/webdav/GABLS3 /home/jovyan/work/remotedir
+sudo mount.davfs https://b2drop.eudat.eu/remote.php/webdav/Jupyter/GABLS3 /home/jovyan/work/data
