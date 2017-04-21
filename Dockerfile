4666d305379ffa80e1ec2b49573870aa881320ef
windbench <pgancarski@cener.com>
2017-04-21 16:50:45 +0200

    Download instead of mount
    The data files will now be download on startup, unless they are already
    available

--- Dockerfile
+++ Dockerfile
@@ -3,28 +3,29 @@ FROM unidata/python
 #switch to root for installing packages
 USER root
                    
-# install davfs2 for being able to mount EUDAT folders
-RUN apt-get update
-RUN apt-get install -yq --no-install-recommends davfs2
-RUN apt-get clean
-RUN rm -rf /var/lib/apt/lists/*
+## install davfs2 for being able to mount EUDAT folders
+#RUN apt-get update
+#RUN apt-get install -yq --no-install-recommends davfs2
+#RUN apt-get clean
+#RUN rm -rf /var/lib/apt/lists/*
 
 # add files directory
+
 
 COPY build_resourcess/ /home/jovyan/work/
 RUN chown -R $NB_USER:users /home/jovyan/work/
 
 COPY eudat_start.sh /usr/local/bin/
-COPY eudat_connect.sh /home/jovyan/work/
+#COPY eudat_connect.sh /home/jovyan/work/
 
-# allow the user to mount eudat folder
-RUN chown root.root /home/jovyan/work/eudat_connect.sh
-RUN chown -R $NB_USER:users /usr/local/bin/eudat_start.sh
+## allow the user to mount eudat folder
+#RUN chown root.root /home/jovyan/work/eudat_connect.sh
+#RUN chown -R $NB_USER:users /usr/local/bin/eudat_start.sh
 
-RUN chmod 4755 /home/jovyan/work/eudat_connect.sh
-RUN chmod +x /usr/local/bin/eudat_start.sh
+#RUN chmod 4755 /home/jovyan/work/eudat_connect.sh
+#RUN chmod +x /usr/local/bin/eudat_start.sh
 
-RUN echo "jovyan  ALL=NOPASSWD: /home/jovyan/work/eudat_connect.sh" >> /etc/sudoers
+#RUN echo "jovyan  ALL=NOPASSWD: /home/jovyan/work/eudat_connect.sh" >> /etc/sudoers
 
 # switch back to the standard user and create a folder for mounting
 USER jovyan
@@ -35,3 +36,5 @@ USER jovyan
 #CMD start-eudat.sh
 
 CMD ["eudat_start.sh"]
+
+
--- eudat_start.sh
+++ eudat_start.sh
@@ -1,9 +1,58 @@
 #!/bin/bash
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
+
+progressfilt ()
+{
+    local flag=false c count cr=$'\r' nl=$'\n'
+    while IFS='' read -d '' -rn 1 c
+    do
+        if $flag
+        then
+            printf '%c' "$c"
+        else
+            if [[ $c != $cr && $c != $nl ]]
+            then
+                count=0
+            else
+                ((count++))
+                if ((count > 1))
+                then
+                    flag=true
+                fi
+            fi
+        fi
+    done
+}
+
+SHARELINK=https://b2drop.eudat.eu/s/gGMXxHJjRwUGe2A
+
+FILES="GABLS3_tendencies_d02_YSU_w60_L9000.nc
+GABLS3_CFDWindSCM_turb5_T2_2.nc
+GABLS3_CFDWindSCM_turb0_T2_9.nc
+GABLS3_CFDWindSCM_turb1_T2_10.nc
+GABLS3_CFDWindSCM_turb5_T2wt_6.nc
+GABLS3_CFDWindSCM_turb5_Tsk_7.nc
+GABLS3_CFDWindSCM_turb5_T2obs_11.nc
+GABLS3_CFDWindSCM_turb5_T2_noTadv_3.nc
+GABLS3_CFDWindSCM_turb5_T2_noTadvUadv_4.nc
+GABLS3_CFDWindSCM_turb5_T2_noTadvUadvSg0_5.nc
+GABLS3_CFDWindSCM_turb5_T2obs_UVTnud2-80_12.nc
+GABLS3_CFDWindSCM_turb5_T2obs_UVTnud2-120_13.nc
+GABLS3_CFDWindSCM_turb5_T2obs_UVTnud2-200_20.nc
+GABLS3_CFDWindSCM_turb5_T2obs_UVTnud2-200tau10_21.nc
+GABLS3_CFDWindSCM_turb5_T2_UVnud40-400_14.nc
+GABLS3_CFDWindSCM_turb5_T2_UVnud40-200_15.nc
+GABLS3_CFDWindSCM_turb5_T2_UVnud40-200tau30_16.nc
+GABLS3_CFDWindSCM_turb5_T2_UVnud40-200tau10_17.nc
+gabls3_scm_cabauw_obs_v33.nc"
+
+for f in $FILES
+do
+	wget --progress=bar:force -nc -O /home/jovyan/work/data/$f $SHARELINK"/download?path=%2F&files="$f 2>&1 | progressfilt
+done
+#curl -s -o /home/jovyan/work/data/gabls3_scm_cabauw_obs_v33.nc 'https://b2drop.eudat.eu/s/HSH5cwfqoeJ76t6/download?path=%2F&files='gabls3_scm_cabauw_obs_v33.nc'  ;return 0
 
 set -e
 
-sudo /home/jovyan/work/eudat_connect.sh
+#sudo /home/jovyan/work/eudat_connect.sh
 
 . /usr/local/bin/start.sh jupyter notebook $*
